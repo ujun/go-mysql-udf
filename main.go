c06880b7f7afb04b2f1d9047a725083333bfc917
Jun Uchino <system.jun@gmail.com>
2015-09-01 19:00:13 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+/*
+#cgo CFLAGS: -I/usr/include/mysql
+#include <mysql.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+*/
+import "C"
+import "unicode/utf8"
+import "os"
+import "os/exec"
+import "fmt"
+
+//export myprint
+func myprint (initid *C.UDF_INIT, args *C.UDF_ARGS, result *C.char, length *C.ulong, is_null *C.char, error *C.char) *C.char {
+  out, err := exec.Command(C.GoString(*args.args)).Output()
+  if err != nil {
+    fmt.Println(err)
+    os.Exit(1)
+  }
+  result = C.CString(string(out))
+  *length = C.ulong(utf8.RuneCountInString(C.GoString(result)))
+  return result
+}
+
+//export myprint_init
+func myprint_init(initid *C.UDF_INIT, args *C.UDF_ARGS, message *C.char) C.my_bool{
+  if(int(args.arg_count) != 1) {
+    return 1
+  } else {
+    return 0
+  }
+}
+
+func main(){
+}
